0fe35bc1eace8aabad2569f368d31d3d09d3e2f5
fotarastm <f.papadopoulos99@hotmail.com>
2023-10-24 14:09:08 +0300

    update text,buttons and string.xml

--- app/src/main/java/com/example/texnologialogismikoucalculator/MainActivity.java
+++ app/src/main/java/com/example/texnologialogismikoucalculator/MainActivity.java
@@ -4,6 +4,9 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
+import org.mariuszgromada.math.mxparser.*;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
