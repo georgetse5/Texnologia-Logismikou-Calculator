13b01e755c09ebbea53cb9a22db0192bf7d07f09
aggelosresitis <aggelos98a@gmail.com>
2023-10-24 11:06:57 +0300

    Added Layout

--- app/src/main/java/com/example/texnologialogismikoucalculator/MainActivity.java
+++ app/src/main/java/com/example/texnologialogismikoucalculator/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.texnologialogismikoucalculator;
-
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    private EditText inputText;
+    private StringBuilder currentInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        inputText = findViewById(R.id.inputText);
+        currentInput = new StringBuilder();
+
+        // Set click listeners for all the buttons
+        setButtonClickListeners();
+    }
+
+    private void setButtonClickListeners() {
+        int[] buttonIds = {
+                R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+                R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
+                R.id.buttonAdd, R.id.buttonSubtract, R.id.buttonMultiply, R.id.buttonDivide,
+                R.id.buttonDot, R.id.buttonClear, R.id.buttonBack, R.id.buttonEquals
+        };
+
+        for (int id : buttonIds) {
+            Button button = findViewById(id);
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onButtonClick((Button) v);
+                }
+            });
+        }
+    }
+
+    private void onButtonClick(Button button) {
+        String buttonText = button.getText().toString();
+        // Handle button click, update the input, and perform calculations
+        // You'll need to implement this based on your calculator's logic.
     }
 }
